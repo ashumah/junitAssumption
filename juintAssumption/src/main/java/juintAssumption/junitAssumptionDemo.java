2368f401cd5d22f2c56cfca8aebd0740890d2598
Your Name <you@example.com>
2022-03-04 18:32:37 +0530

    commited

---
+++ juintAssumption/src/main/java/juintAssumption/junitAssumptionDemo.java
@@ -0,0 +1,30 @@
+package juintAssumption;
+   import static org.junit.jupiter.api.Assertions.assertTrue;
+	import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+	import org.junit.jupiter.api.Test;
+
+	public class junitAssumptionDemo {
+	    @Test
+	    public void onlyOnWindows() {
+	        assumeTrue(isWindows());
+	        assertTrue(true);
+	    }
+
+	    @Test
+	    public void notOnWindows() {
+	        assumeTrue(!isWindows());
+	        assertTrue(true);
+	    }
+
+	    private static boolean isWindows() {
+	        String osType = System.getProperty("os.name");
+	        if (osType.contains("windows") || osType.contains("Windows") || osType.contains("WINDOWS")) {
+	            return true;
+	        }
+	        return false;
+
+
+}
+}
+
